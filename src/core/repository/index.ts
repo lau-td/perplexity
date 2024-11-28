4ee125fe8205a42719af7db35eb3771de05a1ad9
nguyenphuvinhtoan <nguyenphuvinhtoan@gmail.com>
2024-11-28 09:33:52 +0700

    refactor: change index.ts

--- src/core/repository/index.ts
+++ src/core/repository/index.ts
@@ -3,5 +3,5 @@ export * from './youtube.repository.interface';
 export * from './embedding.repository.interface';
 export * from './document.repository.interface';
 export * from './document-segment.repository.interface';
-export * from './flash-card-repository.interface';
+export * from './flash-card.repository.interface';
 export * from './user-repository.interface';
